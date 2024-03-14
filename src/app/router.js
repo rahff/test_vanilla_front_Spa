cb7de1a2d47a4c8a92f87801f744a2c75a5ded76
rahff <raphaelandrey99@gmail.com>
2024-03-14 23:06:45 +0100

    fix fakeElement missing method on component test

--- src/app/router.js
+++ src/app/router.js
@@ -1,27 +1,7 @@
-import {
-    addTodoInList,
-    doneTodoInList,
-    makeTodo,
-    removeTodoInList,
-    todoListQuery
-} from "../core/todoList/todo.use-cases.js";
 import {store} from "../core/store.js";
-import {todoListView} from "./views/todoList.view.js";
-import {fetchCard, fetchTodos, idProvider} from "../adapters/adapters.js";
-import {cardListView} from "./views/cardList.view.js";
-import {queryCards} from "../core/cards/cards.use-cases.js";
 import {queryView} from "./utils.js";
 
-const todoListProvider = {
-    addTodo: addTodoInList(store, makeTodo(idProvider)),
-    deleteTodo: removeTodoInList(store),
-    doneTodo: doneTodoInList(store),
-    query: todoListQuery(store, fetchTodos)
-};
 
-const cardListProvider = {
-    query: queryCards(store, fetchCard)
-}
 
 export class Router {
     #state = "/";
@@ -39,16 +19,16 @@ export class Router {
         return Router.#INSTANCE;
     }
 
+    navigate(path){
+        this.#state = path;
+        this.#init();
+    }
+
     #init(){
         const route = this.#routes.find(route => route.path === this.#state)
         if(!route) throw new Error("unknown path");
         if(this.#storeUnsubscribe) this.#storeUnsubscribe();
         this.#storeUnsubscribe = initView(route, store);
-    }
-
-    navigate(path){
-        this.#state = path;
-        this.#init();
     }
 }
 
